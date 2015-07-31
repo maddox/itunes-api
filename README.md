f1141899c9cc8288a03ccb0d9732e2c8314847df
Jon Maddox <jon@jonmaddox.com>
2015-07-31 01:22:00 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,106 @@
+# iTunes API!!
+
+iTunes API is a simple REST server allowing you to query/control a local running
+iTunes instance. Once you have a way to make RESTful API calls to control iTunes,
+you can integrate its control into any of your own software.
+
+This is especially convenient for integrating into any sort of Home Automationyou might have.
+
+Last confirmed working version of iTunes: `12.2.1`.
+
+
+## Features
+
+* play/stop/pause/playpause/next/previous control over what's playing.
+* query to return what's currently playing.
+* fetch the art of the currently playing song.
+* set a specific playlist to play, by name.
+* query to return a list of available AirPlay endpoints.
+* set an AirPlay endpoint to be active. (This can be multiple, since iTunes
+  supports it).
+
+## Setup
+
+    npm install
+    npm run start
+
+## Docs
+
+This is a quick overview of the service. Read [app.js](app.js) if you need more
+info.
+
+### Resources
+
+Here's a list of resources that may be returned in a response.
+
+#### NowPlaying Resource
+
+The NowPlaying resource returns all the information about if iTunes is playing
+and what is playing.
+
+```json
+{
+  "player_state": "playing",
+  "id": "896CD158E5AB7CEC",
+  "name": "Dreams",
+  "artist": "Beck",
+  "album": "Dreams - Single (2015)"
+}```
+
+#### AirPlayDevice Resource
+
+The AirPlayDevice resource returns all the information about an available
+AirPlay device on your network.
+
+```json
+{
+  "id": 108649,
+  "name": "Living Room",
+  "kind": "Apple TV",
+  "active": true,
+  "selected": true,
+  "soundVolume": 60,
+  "supportsVideo": true,
+  "supportsAudio": true,
+  "networkAddress": "63:22:fa:1f:f5:d4"
+}
+```
+
+### Methods
+
+These are the endpoints you can hit to do things.
+
+#### Player Control
+  Use these endpoints to control what's currently playing.
+
+    PUT /playpause => NowPlayingResource
+    PUT /stop => NowPlayingResource
+    PUT /previous => NowPlayingResource
+    PUT /next => NowPlayingResource
+    PUT /play => NowPlayingResource
+    PUT /play => NowPlayingResource
+
+#### Info
+  Use these endpoints to query the current state of iTunes.
+
+    GET /now_playing => NowPlayingResource
+    GET /artwork => JPEG Data (image/jpeg)
+
+#### Playlist Control
+  Use this endpoint to start a specific playlist.
+
+    PUT /play_playlist?playlist="Party%20Time" => NowPlayingResource
+
+#### AirPlay Control
+  Use these endpoints to query and set AirPlay devices. You can set multiple
+  AirPlay devices to be used at the same time.
+
+    GET /airplay_devices => {:airplay_devices => [AirPlayDevice, AirPlayDevice, ...]}
+    PUT /airplay_devices/:id/on => AirPlayDevice
+    PUT /airplay_devices/:id/off => AirPlayDevice
+
+## Contributions
+
+* fork
+* create a feature branch
+* open a Pull Request
---
+++ app.js
@@ -0,0 +1,143 @@
+var fs = require('fs')
+var path = require('path')
+var util = require('util')
+var express = require('express')
+var bodyParser = require('body-parser')
+var iTunes = require('local-itunes')
+var osa = require('osa')
+var osascript = require(path.join(__dirname, 'node_modules', 'local-itunes', 'node_modules', 'osascript'))
+var airplay = require('./lib/airplay')
+
+var app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+
+function getCurrentState(){
+  itunes = Application('iTunes');
+  currentTrack = itunes.currentTrack;
+  playerState = itunes.playerState();
+  currentState = {};
+
+  currentState['player_state'] = playerState;
+  currentState['id'] = currentTrack.persistentID();
+  currentState['name'] = currentTrack.name();
+  currentState['artist'] = currentTrack.artist();
+  currentState['album'] = currentTrack.album();
+
+  if (currentTrack.year()) {
+    currentState['album'] += " (" + currentTrack.year() + ")";
+  }
+
+  return currentState;
+}
+
+function sendResponse(error, res){
+  if (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }else{
+    osa(getCurrentState, function (error, state) {
+      console.log(error)
+      if (error) {
+        res.sendStatus(500)
+      }else{
+        res.json(state)
+      }
+    })
+  }
+}
+
+function playPlaylist(name){
+  itunes = Application('iTunes');
+  itunes.playlists.byName(name).play();
+
+  return true;
+}
+
+app.put('/play', function(req, res){
+  iTunes.play(function (error){
+    sendResponse(error, res)
+  })
+})
+
+app.put('/pause', function(req, res){
+  iTunes.pause(function (error){
+    sendResponse(error, res)
+  })
+})
+
+app.put('/playpause', function(req, res){
+  iTunes.playpause(function (error){
+    sendResponse(error, res)
+  })
+})
+
+app.put('/stop', function(req, res){
+  iTunes.stop(function (error){
+    sendResponse(error, res)
+  })
+})
+
+app.put('/previous', function(req, res){
+  iTunes.previous(function (error){
+    sendResponse(error, res)
+  })
+})
+
+app.put('/next', function(req, res){
+  iTunes.next(function (error){
+    sendResponse(error, res)
+  })
+})
+
+app.get('/now_playing', function(req, res){
+  error = null
+  sendResponse(error, res)
+})
+
+app.get('/artwork', function(req, res){
+  osascript.file(path.join(__dirname, 'lib', 'art.applescript'), function (error, data) {
+    res.type('image/jpeg')
+    res.sendFile('/tmp/currently-playing.jpg')
+  })
+})
+
+app.post('/play_playlist', function (req, res) {
+  osa(playPlaylist, req.body.playlist, function (error, data) {
+    sendResponse(error, res)
+  })
+})
+
+app.get('/airplay_devices', function(req, res){
+  osa(airplay.listAirPlayDevices, function(error, data, log){
+    if (error){
+      res.sendStatus(500)
+    }else{
+      res.json({'airplay_devices': data})
+    }
+  })
+})
+
+app.put('/airplay_devices/:id/on', function (req, res) {
+  osa(airplay.setSelectionStateAirPlayDevice, req.params.id, true, function(error, data, log){
+    if (error){
+      console.log(error)
+      res.sendStatus(500)
+    }else{
+      res.json(data)
+    }
+  })
+})
+
+app.put('/airplay_devices/:id/off', function (req, res) {
+  osa(airplay.setSelectionStateAirPlayDevice, req.params.id, false, function(error, data, log){
+    if (error){
+      console.log(error)
+      res.sendStatus(500)
+    }else{
+      res.json(data)
+    }
+  })
+})
+
+
+app.listen(process.env.PORT || 8181);
